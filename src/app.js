7ee081d35e7c5981411f43436ebe4937ebc45f40
GulcP <gulcinparmentier@gmail.com>
2024-09-07 23:55:19 +0200

    Forecast api added

--- src/app.js
+++ src/app.js
@@ -57,28 +57,38 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=ca4bof60fdf24464tc7e37793a25dcc2&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHTML = "";
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="weather-forecast-day">
-      <div class="weather-forecast-date">${day}</div>
-                <div class="weather-forecast-icon">🌤</div>
-                <div class="weather-forecast-temperatures">
-                    <div class="weather-forecast-temperature"> <strong></strong> </div>
-                    <div class="weather-forecast-temperature"></div>
-                </div>
-                `;
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="weather-forecast-day">
+
+      <div class="weather-forecast-date">${formatDay(day.time)} </div>
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+      <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+      <strong>${Math.round(day.temperature.maximum)}º</strong> </div>
+      <div class="weather-forecast-temperature">${Math.round(
+        day.temperature.minimum
+      )}º</div>
+      </div>
+      </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
